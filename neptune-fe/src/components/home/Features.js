613b09b5272c97740a43b18b930d9f78847cf54d
Zach Molony <31472522+zachmolony@users.noreply.github.com>
2022-07-29 20:11:55 +0100

    [NEP-41] - update home page

---
+++ neptune-fe/src/components/home/Features.js
@@ -0,0 +1,17 @@
+const Features = () => {
+  return (
+    <div className="">
+      <h2 className="text-3xl font-bold">Headless Commerce</h2>
+      <ul>
+        <li>Transaction based fees</li>
+        <li>Create, edit, and manage products</li>
+        <li>Payment methods</li>
+        <li>Order management</li>
+        <li>Prevent overselling</li>
+        <li>Manage multiple client sites</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Features;
--- neptune-fe/src/pages/Home.js
+++ neptune-fe/src/pages/Home.js
@@ -10,7 +10,8 @@ import Tools from "../assets/tools.jpeg";
 import HomeBar from "../components/home/HomeBar";
 import SalesContent from "../components/home/SalesContent";
 import PageWrapper from "../components/home/PageWrapper";
-import Footer from "../components/Footer";
+import Footer from "../components/home/Footer";
+import Features from "../components/home/Features";
 
 const content = {
   build: {
@@ -19,7 +20,7 @@ const content = {
     img: Rocketship,
     alt: "Rocket ship",
     subtitle:
-      "Everything your client needs to build and manage their business - while earning you revenue."
+      "Everything your client needs to build and manage their business - while earning you revenue.",
   },
   performance: {
     headline: "Fees that work for both your client, and ",
@@ -27,7 +28,7 @@ const content = {
     img: Performance,
     alt: "Performance Graph",
     subtitle:
-      "Neptune’s micro-commission model enables you to charge fees on each transaction on your client's site. "
+      "Neptune’s micro-commission model enables you to take a cut of transactions on your client's site. ",
   },
   tools: {
     headline: "Manage multiple clients from the ",
@@ -35,8 +36,8 @@ const content = {
     img: Tools,
     alt: "Tools",
     subtitle:
-      "Neptune’s developer interface makes it easy to manage multiple client platforms at a time."
-  }
+      "Neptune’s developer interface makes it easy to build and manage multiple client platforms at a time.",
+  },
 };
 
 const Home = () => {
@@ -48,11 +49,7 @@ const Home = () => {
   return (
     <>
       <div className="h-full w-full bg-secondary text-white">
-        <img
-          src={Stars}
-          alt=""
-          className="w-full h-screen opacity-40 absolute object-cover"
-        />
+        <img src={Stars} alt="" className="w-full h-screen opacity-40 absolute object-cover" />
 
         <div className="w-full h-screen absolute flex">
           <div className="w-full relative">
@@ -77,8 +74,8 @@ const Home = () => {
               </h1>
             </div>
             <div className="w-1/2 mt-auto 2xl:pl-32">
-              <h2 className="pl-24 pr-8 lg:pl-32 ml-12 pb-32 font-bold text-2xl xl:text-3xl">
-                The e-commerce platform built by developers, for developers.
+              <h2 className="pl-24 pr-8 lg:pl-32 ml-12 pb-32 font-bold text-2xl">
+                The free e-commerce platform built by developers, for developers.
               </h2>
             </div>
           </div>
@@ -90,6 +87,12 @@ const Home = () => {
             <SalesContent {...content[page]} flipped={!!(index % 2)} />
           </PageWrapper>
         ))}
+
+        {false && (
+          <PageWrapper>
+            <Features />
+          </PageWrapper>
+        )}
 
         <PageWrapper>
           <div className="w-7/12" ref={textScroll.ref}>
